da7c2535125329629d875e57544005445f7e2ea7
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-01-16 09:22:09 +0100

    segunda parte rproductor

--- Tema3/reproductorAudio/funcionalidad.js
+++ Tema3/reproductorAudio/funcionalidad.js
@@ -1,4 +1,21 @@
 var audio=document.getElementById("audio");
-audio.play();
+var boton_play=document.getElementById("play");
+var boton_stop=document.getElementById("stop");
+var boton_pausa=document.getElementById("pausa");
+var boton_reiniciar=document.getElementById("reiniciar");
+var boton_repetir=document.getElementById("loop");
+var volume_off = document.getElementById("volume-off");
+var volume_on = document.getElementById("volume-on");
+var bajar_volumen = document.getElementById("bajar-volumen");
+var subir_volumen = document.getElementById("subir-volumen");
 
 
+function play() {
+    if(audio.paused){
+        audio.play;
+    }
+}
+function pausa() {
+    audio.paused;
+    
+}
