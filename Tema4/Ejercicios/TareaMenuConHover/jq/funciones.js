a9f832b216f8987f1d7f1b1c0956c2918f0eb509
jsalgua027 <jsalcedoguarde@gmail.com>
2024-02-08 09:49:41 +0100

    terminado tarea3

--- Tema4/Ejercicios/TareaMenuConHover/jq/funciones.js
+++ Tema4/Ejercicios/TareaMenuConHover/jq/funciones.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
-    $("ul#op-menu>li").hover(function () {
-        $(this).children("a").css("backgroundColor", "red")
-    })
-
+    $(" header nav#main-menu ul#op-menu>li").hover(function () {
+        $(this).children("a").css({"background-color": "red","font-weight": "bold"})
+    },
+    function() {
+        $(this).children("a").css({"background-color": "","font-weight": ""})
     
+    })
+  
+   
 });
+
+/* $(document).ready(function() {
+    $("#menu li").hover(
+      function() {
+        // Al pasar el ratón por encima
+        $(this).css({
+          "background-color": "lightblue",
+          "font-weight": "bold"
+        });
+      },
+      function() {
+        // Al dejar de estar encima
+        $(this).css({
+          "background-color": "", // Restaura el color de fondo
+          "font-weight": "" // Restaura el estilo de fuente
+        });
+      }
+    );
+  }); */
