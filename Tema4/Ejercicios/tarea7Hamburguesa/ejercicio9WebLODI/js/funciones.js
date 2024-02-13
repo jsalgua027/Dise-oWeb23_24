6e70547e13db762a5581f1f368067a32bbf85adc
jsalgua027 <jsalcedoguarde@gmail.com>
2024-02-13 14:23:02 +0100

    tarea 7 y 8 terminadas

---
+++ Tema4/Ejercicios/tarea7Hamburguesa/ejercicio9WebLODI/js/funciones.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  var header = $("header"); /**se utiliza para seleccionar el elemento <header>
+     en el documento HTML y almacenarlo en la variable header */
+
+  $(window).scroll(function () {
+    var scroll =
+      $(
+        window
+      ).scrollTop(); /**se utiliza para obtener la posición actual de desplazamiento vertical (scroll)
+           de la ventana del navegador,Cuando el usuario hace scroll hacia abajo,
+            el valor de scroll aumenta. Este valor se utiliza en el código siguiente para determinar
+            si la posición de desplazamiento es mayor que 100 píxeles
+            */
+
+    if (scroll > 100) {
+      //si es mayor de 100 píxeles me añades la clase fixed-transparent
+      header.addClass("fixed-transparent");
+    } else {
+      // quitas la clase
+      header.removeClass("fixed-transparent");
+    }
+  });
+});
+
+
+
+$(document).ready(function () {
+  $("span").on("click", function () {
+    $("header>nav#menu-principal>input,header>nav#menu-principal>ul#menu").toggle()
+    
+  });
+  
+
+});
+$(document).ready(function () {
+  $(window).on("resize", function () {
+  
+    $("header>nav#menu-principal>input,header>nav#menu-principal>ul#menu").css("display","none"); 
+  });
+});
