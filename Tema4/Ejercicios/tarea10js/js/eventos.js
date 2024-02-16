4e23a18c3ec05b390434e3141bc27a0b751a5cb6
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-02-16 17:13:56 +0100

    ejercicio 10 cambios

--- Tema4/Ejercicios/tarea10js/js/eventos.js
+++ Tema4/Ejercicios/tarea10js/js/eventos.js
@@ -1,10 +1,21 @@
-$(document).ready(function(){
-    $("smain>article>div>section>div>div>svg").click(function(e){
-        console.log("entra")
-        $("div.texto>p").faceOut();
-    });
-   // $("main>article>div>section>div>div>svg:nth-child(2)").click(function(e){
-   //     $("div.texto>p").faceIn();
-   // })
-
+$(function(){
+    let estado=false;
+$("main>article>div>section>div").click(function(e){
+  
+    if(estado){
+        $(this).next().fadeOut();
+        console.log(estado);
+        /* transform: rotate(90deg);
+    transition: transform 0.3s ease;*/
+       
+    } else {
+        $(this).next().fadeIn();
+        console.log(estado);
+      
+    }
+            
+               
+estado=!estado;
+})
+    
 })
