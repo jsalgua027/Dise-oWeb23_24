4f584ed8d6f9f09b3a7182821115e3eacc244773
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-02-17 20:37:55 +0100

    tarea 10 lista

--- Tema4/Ejercicios/tarea10js/js/eventos.js
+++ Tema4/Ejercicios/tarea10js/js/eventos.js
@@ -1,16 +1,23 @@
 $(function(){
+    //escondo los elementos en la carga de la web
+    $("body > main > article > div > section > div.texto ").css('display','none');
+
     let estado=false;
 $("main>article>div>section>div").click(function(e){
   
-    if(estado){
-        $(this).next().fadeOut();
+    if(estado){ 
+        e.preventDefault();
+        $(this).next().fadeOut(300);
         console.log(estado);
-        /* transform: rotate(90deg);
-    transition: transform 0.3s ease;*/
+        $(this).find('div > svg:eq(1)').css({"transform": "rotate(90deg)",
+        "transition": "transform 0.3s ease"});
        
     } else {
-        $(this).next().fadeIn();
+        e.preventDefault();
+        $(this).next().fadeIn(300);
         console.log(estado);
+        $(this).find('div > svg:eq(1)').css({"transform": "rotate(0deg)",
+        "transition": "transform 0.3s ease"});
       
     }
             
