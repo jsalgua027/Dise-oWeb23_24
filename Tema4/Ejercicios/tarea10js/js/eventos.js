d9cafacb436fba9ab6e1ec9479ddd5fabf14a0eb
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-02-17 20:43:28 +0100

    comentarios

--- Tema4/Ejercicios/tarea10js/js/eventos.js
+++ Tema4/Ejercicios/tarea10js/js/eventos.js
@@ -9,15 +9,15 @@ $("main>article>div>section>div").click(function(e){
         e.preventDefault();
         $(this).next().fadeOut(300);
         console.log(estado);
-        $(this).find('div > svg:eq(1)').css({"transform": "rotate(90deg)",
-        "transition": "transform 0.3s ease"});
+        $(this).find('div > svg:nth-child(2)').css({"transform": "rotate(90deg)",
+        "transition": "transform 0.4s ease"});
        
     } else {
         e.preventDefault();
         $(this).next().fadeIn(300);
         console.log(estado);
-        $(this).find('div > svg:eq(1)').css({"transform": "rotate(0deg)",
-        "transition": "transform 0.3s ease"});
+        $(this).find('div > svg:nth-child(2)').css({"transform": "rotate(0deg)",
+        "transition": "transform 0.4s ease"});
       
     }
             
@@ -26,3 +26,8 @@ estado=!estado;
 })
     
 })
+/*
+find('div > svg:eq(1)')  otro selector es como un array
+                         svg:eq(0) el primer elemento, svg:eq(1) es el segundo elemento
+
+*/
