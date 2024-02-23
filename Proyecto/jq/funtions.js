c27e68eac27e298bc7f36490657d2c24f3571229
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-02-23 20:26:44 +0100

    añadido funcinalidad jqueri y menú desplegable

---
+++ Proyecto/jq/funtions.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function() {
+    $("div#menu_header svg").on("click", function(){
+        console.log("entra")
+        $("nav#menu_desple").toggle("slow"); // parte 2
+    
+    })
+});
+
+
