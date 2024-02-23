a67a6f50957eefaa809fda59a84858beb7c6761c
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-02-23 20:43:06 +0100

    cambiado enlaces de las imagenes en todos los html

--- Proyecto/jq/funtions.js
+++ Proyecto/jq/funtions.js
@@ -1,6 +1,8 @@
 
 
 $(document).ready(function() {
+     $("nav#menu_desple").css({'display':'none'})
+     
     $("div#menu_header svg").on("click", function(){
         console.log("entra")
         $("nav#menu_desple").toggle("slow"); // parte 2
