43cd12705c1a4af32dd1b97670fcc76acf1e3e7b
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-02-29 14:10:50 +0100

    cambios

--- Proyecto/jq/funtions.js
+++ Proyecto/jq/funtions.js
@@ -2,27 +2,48 @@
 
 $(document).ready(function() {
      $("nav#menu_desple").css({'display':'none'})
+     $("div#login_desplegue").css({'display':'none'})
+     $("div#crearCuenta_desple").css({'display':'none'})
      
     $("div#menu_header svg").on("click", function(){
-        console.log("entra")
+       
         $("nav#menu_desple").toggle("slow"); // parte 2
     
     })
+    $("nav#menu-tablet-escrito svg").on("click", function(){
+      $("div#login_desplegue").slideToggle("slow"); // parte 2
+ 
+     
+  })
 
-    $('svg.iniciar').click(function() {
+
+
+
+ $('svg.iniciar').click(function() {
         // Redirige a la URL deseada
-        console.log("entra")
+       
         window.location.href = 'login.html';
       });
 
       
     $('svg.iniciar_Ini').click(function() {
         // Redirige a la URL deseada
-        console.log("entra")
+        
         window.location.href = 'Pages/login.html';
       });
 
    
+      $('nav#menu-tablet-escrito button').click(function() {
+        // Redirige a la URL deseada
+        console.log("entra")
+        window.location.href = 'Inicio.html';
+      });
+  
+
+
+   
+
+
 
 });
 
