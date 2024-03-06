cbf77ff441fcbeee17fa1a60f74c46bca869422b
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-03-06 20:57:32 +0100

    prueba de examen ejercicio1 c

---
+++ webPrueba/ExamenesJquey/printodecor-alumnos/printodecor-alumnos/jq/funciones.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function() {
+    var mostrar=true;
+        $("#hamburger").on('click', function() {
+            /*if(mostrar){
+                console.log("entra");
+                $("ul#menu-toggle").stop(true).fadeIn()
+                mostrar=false;
+            }else{
+                $("ul#menu-toggle").stop(true).fadeOut()
+                mostrar=true;
+            }*/
+            /***ejercicio 1 */
+
+        $("ul#menu-toggle").stop().slideToggle(400) // efecto cortinilla  A
+            //B  y //C
+            if (mostrar) {
+                $("#hamburger > span:nth-child(2)").stop(true).animate({ width: "67%" });
+                $("#hamburger").stop(true).animate({ backgroundColor: "rgba(255, 0, 0, 0.5)" }, 400); // Cambia a rojo de forma progresiva
+                mostrar = false;
+            } else {
+                $("#hamburger > span:nth-child(2)").stop(true).animate({ width: "40%" });
+                $("#hamburger").stop(true).animate({ backgroundColor: "#e3e3e3" }, 400); // Restaura el color original de forma progresiva
+                mostrar = true;
+            }
+       
+
+    });
+
+    $("#menu-toggle > li:nth-child(3) > a").on('click',function(){
+        console.log("Entra")
+        var submenu = $("#menu-toggle > li:nth-child(3) > ul");
+    
+        if (submenu.is(":visible")) {
+            // Si el menú está visible, lo ocultamos
+            submenu.stop(true).animate({ width: "50px" }, 400, function() {
+                $(this).hide(); // Oculta el menú después de la animación
+            });
+        } else {
+            // Si el menú está oculto, lo mostramos
+            submenu.show().stop(true).animate({ width: "150px" }, 400);
+        }
+
+    })
+
+
+
+
+});
